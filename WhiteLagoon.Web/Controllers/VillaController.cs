ef7d465b2d8885b626bfbd2657912e4c24c64dd9
Art'm <kasko.artem45@gmail.com>
2026-03-16 16:41:35 +0300

    Create VillaController and retrieve all villas

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace BulkyWeb.Controllers;
+
+public class VillaController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public VillaController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var villas = _db.Villas.ToList();
+        return View(villas);
+    }
+}
