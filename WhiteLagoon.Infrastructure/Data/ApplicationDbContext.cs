11a31ac74d167d2732a91a0a00f387f5a0777b1d
Art'm <kasko.artem45@gmail.com>
2026-03-16 15:47:22 +0300

    Create Db and Villa Table

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Infrastructure.Data;
 
@@ -7,4 +8,6 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+    
+    public DbSet<Villa> Villas { get; set; }
 }
