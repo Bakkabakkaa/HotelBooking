b33c2d79b1a644a438197031410f6a26b6912a5c
Art'm <kasko.artem45@gmail.com>
2026-03-19 03:19:29 +0300

    TempData Notifications

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -39,6 +39,7 @@ public class VillaController : Controller
         {
             _db.Villas.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "The villa has been created successfully.";
             return RedirectToAction("Index", "Villa");
         }
 
@@ -65,6 +66,7 @@ public class VillaController : Controller
         {
             _db.Villas.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "The villa has been updated successfully.";
             return RedirectToAction("Index");
         }
 
@@ -93,9 +95,11 @@ public class VillaController : Controller
         {
             _db.Villas.Remove(objFromDb);
             _db.SaveChanges();
+            TempData["success"] = "The villa has been deleted successfully.";
             return RedirectToAction("Index");
         }
 
+        TempData["error"] = "The villa could not be deleted";
         return View(); 
     }
     
