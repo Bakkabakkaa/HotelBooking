2e6997c5b7722498dedaf24c71b31500ff70bb56
Art'm <kasko.artem45@gmail.com>
2026-03-20 17:39:02 +0300

    Using Villa Repository in Controller

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
@@ -7,17 +8,17 @@ namespace WhiteLagoon.Web.Controllers;
 
 public class VillaController : Controller
 {
-    private readonly ApplicationDbContext _db;
+    private readonly IVillaRepository _villaRepo;
 
-    public VillaController(ApplicationDbContext db)
+    public VillaController(IVillaRepository villaRepo)
     {
-        _db = db;
+        _villaRepo = villaRepo;
     }
 
     [HttpGet]
     public IActionResult Index()
     {
-        var villas = _db.Villas.ToList();
+        var villas = _villaRepo.GetAll();
         return View(villas);
     }
 
@@ -37,8 +38,8 @@ public class VillaController : Controller
         
         if (ModelState.IsValid)
         {
-            _db.Villas.Add(obj);
-            _db.SaveChanges();
+            _villaRepo.Add(obj);
+            _villaRepo.Save();
             TempData["success"] = "The villa has been created successfully.";
             return RedirectToAction("Index", "Villa");
         }
@@ -49,7 +50,7 @@ public class VillaController : Controller
     [HttpGet]
     public IActionResult Update(int villaId)
     {
-        Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+        Villa? obj = _villaRepo.Get(u => u.Id == villaId);
 
         if (obj == null)
         {
@@ -64,8 +65,8 @@ public class VillaController : Controller
     {
         if (ModelState.IsValid && obj.Id > 0)
         {
-            _db.Villas.Update(obj);
-            _db.SaveChanges();
+            _villaRepo.Update(obj);
+            _villaRepo.Save();
             TempData["success"] = "The villa has been updated successfully.";
             return RedirectToAction(nameof(Index));
         }
@@ -76,7 +77,7 @@ public class VillaController : Controller
     [HttpGet]
     public IActionResult Delete(int villaId)
     {
-        Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+        Villa? obj = _villaRepo.Get(u => u.Id == villaId);
 
         if (obj is null)
         {
@@ -89,12 +90,12 @@ public class VillaController : Controller
     [HttpPost]
     public IActionResult Delete(Villa obj)
     {
-        Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+        Villa? objFromDb = _villaRepo.Get(u => u.Id == obj.Id);
         
         if (objFromDb is not null)
         {
-            _db.Villas.Remove(objFromDb);
-            _db.SaveChanges();
+            _villaRepo.Remove(objFromDb);
+            _villaRepo.Save();
             TempData["success"] = "The villa has been deleted successfully.";
             return RedirectToAction(nameof(Index));
         }
