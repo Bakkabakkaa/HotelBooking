c900797e9ad8af504cf0983466bb0bc842ea0456
Art'm <kasko.artem45@gmail.com>
2026-03-22 21:02:52 +0300

    Index Action Home Controller

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -1,14 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Web.Models;
+using WhiteLagoon.Web.ViewModels;
 
 namespace WhiteLagoon.Web.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public HomeController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
     public IActionResult Index()
     {
-        return View();
+        HomeVM homeVm = new HomeVM()
+        {
+            VillaList = _unitOfWork.Villa.GetAll(),
+            Nights = 1,
+            CheckInDate = DateOnly.FromDateTime(DateTime.Now)
+        };
+
+        return View(homeVm);
+
     }
 
     public IActionResult Privacy()
