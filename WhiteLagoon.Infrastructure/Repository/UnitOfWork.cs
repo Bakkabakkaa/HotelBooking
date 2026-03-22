2c8e7fd9b99d340f808bc051b3e2fe57d5f11f09
Art'm <kasko.artem45@gmail.com>
2026-03-22 19:21:23 +0300

    Amenity Repository

--- WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
+++ WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
@@ -8,12 +8,14 @@ public class UnitOfWork : IUnitOfWork
     private readonly ApplicationDbContext _db;
     public IVillaRepository Villa { get; private set; }
     public IVillaNumberRepository VillaNumber { get; }
+    public IAmenityRepository Amenity { get; }
 
     public UnitOfWork(ApplicationDbContext db)
     {
         _db = db;
         Villa = new VillaRepository(_db);
         VillaNumber = new VillaNumberRepository(_db);
+        Amenity = new AmenityRepository(_db);
     }
 
     public void Save()
