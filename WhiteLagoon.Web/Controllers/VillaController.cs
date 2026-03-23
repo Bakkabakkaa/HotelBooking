785387ecbdc68995d938cc6fd914c5d97f7a4613
Art'm <kasko.artem45@gmail.com>
2026-03-24 00:36:17 +0300

    Authorization with Roles

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WhiteLagoon.Application.Common.Interfaces;
@@ -6,6 +7,7 @@ using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers;
 
+[Authorize]
 public class VillaController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
