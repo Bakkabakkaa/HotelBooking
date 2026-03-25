309f4ac56762e2ab5835dd410df88eef86f40b37
Art'm <kasko.artem45@gmail.com>
2026-03-25 18:58:54 +0300

    More Methods in Booking Repository

--- WhiteLagoon.Infrastructure/Repository/BookingRepository.cs
+++ WhiteLagoon.Infrastructure/Repository/BookingRepository.cs
@@ -1,4 +1,6 @@
+using System.Runtime.InteropServices.JavaScript;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
@@ -16,5 +18,43 @@ public class BookingRepository : Repository<Booking>, IBookingRepository
     public void Update(Booking entity)
     {
         _db.Bookings.Update(entity);
+    }
+
+    public void UpdateStatus(int bookingId, string bookingStatus)
+    {
+        var bookingFromDb = _db.Bookings.FirstOrDefault(m => m.Id == bookingId);
+        if (bookingFromDb != null)
+        {
+            bookingFromDb.Status = bookingStatus;
+            if (bookingStatus == SD.StatusCheckedIn)
+            {
+                bookingFromDb.ActualCheckInDate = DateTime.Now;
+            }
+
+            if (bookingStatus == SD.StatusCompleted)
+            {
+                bookingFromDb.ActualCheckOutDate = DateTime.Now;
+            }
+        }
+    }
+
+    public void UpdateStripePaymentID(int bookingId, string sessionId, string paymentIntentId)
+    {
+        var bookingFromDb = _db.Bookings.FirstOrDefault(m => m.Id == bookingId);
+
+        if (bookingFromDb != null)
+        {
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                bookingFromDb.StripeSessionId = sessionId;
+            }
+
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                bookingFromDb.StripePaymentIntentId = paymentIntentId;
+                bookingFromDb.PaymentDate = DateTime.Now;
+                bookingFromDb.IsPaymentSuccessful = true;
+            }
+        }
     }
 }
