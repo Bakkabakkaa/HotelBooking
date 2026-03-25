c651e9a78ef048f48fa3b20033b8f77b2adaafee
Art'm <kasko.artem45@gmail.com>
2026-03-25 18:08:24 +0300

    Solve Bugs with Finalize Booking

---
+++ WhiteLagoon.Web/Views/Booking/BookingConfirmation.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WhiteLagoon.Web.Views.Booking;
+
+public class BookingConfirmation : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
