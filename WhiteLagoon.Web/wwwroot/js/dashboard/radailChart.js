d28f4ab0564e8692554ed1c5b67c19a951b19d63
Art'm <kasko.artem45@gmail.com>
2026-03-28 22:13:15 +0300

    Pie chart in action

--- WhiteLagoon.Web/wwwroot/js/dashboard/radailChart.js
+++ WhiteLagoon.Web/wwwroot/js/dashboard/radailChart.js
@@ -1,3 +1,4 @@
+
 function loadRadialBarChart(id, data) {
     var chartColors = getChartColorsArray(id);
     var options = {
@@ -29,21 +30,22 @@ function loadRadialBarChart(id, data) {
     var chart = new ApexCharts(document.querySelector("#" + id), options);
     chart.render();
 
-    function getChartColorsArray(id) {
-        if (document.getElementById(id) !== null) {
-            var colors = document.getElementById(id).getAttribute("data-colors");
-            if (colors) {
-                colors = JSON.parse(colors);
-                return colors.map(function (value) {
-                    var newValue = value.replace(" ", "");
-                    if (newValue.indexOf(",") === -1) {
-                        var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
-                        if (color) return color;
-                        else return newValue;
-                    }
-                });
-            }
+}
 
+function getChartColorsArray(id) {
+    if (document.getElementById(id) !== null) {
+        var colors = document.getElementById(id).getAttribute("data-colors");
+        if (colors) {
+            colors = JSON.parse(colors);
+            return colors.map(function (value) {
+                var newValue = value.replace(" ", "");
+                if (newValue.indexOf(",") === -1) {
+                    var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+                    if (color) return color;
+                    else return newValue;
+                }
+            });
         }
+
     }
 }
