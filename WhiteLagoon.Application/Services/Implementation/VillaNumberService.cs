a4b4ce7e5b377ff977a68be1ea42565e82b9c4ab
Art'm <kasko.artem45@gmail.com>
2026-03-31 00:57:02 +0300

    VillaNumber Service

---
+++ WhiteLagoon.Application/Services/Implementation/VillaNumberService.cs
@@ -0,0 +1,56 @@
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Services.Interface;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Services.Implementation;
+
+public class VillaNumberService : IVillaNumberService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public VillaNumberService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public void CreateVillaNumber(VillaNumber villaNumber)
+    {
+        _unitOfWork.VillaNumber.Add(villaNumber);
+        _unitOfWork.Save();
+    }
+
+    public bool DeleteVillaNumber(int id)
+    {
+        try
+        {
+            VillaNumber? objFromDb = _unitOfWork.VillaNumber.Get(u => u.Villa_Number == id);
+            if (objFromDb is not null)
+            {
+                _unitOfWork.VillaNumber.Remove(objFromDb);
+                _unitOfWork.Save();
+                return true;
+            }
+            return false;
+        }
+        catch (Exception)
+        {
+            return false;
+        }   
+    }
+
+    public IEnumerable<VillaNumber> GetAllVillaNumbers()
+    {
+        return _unitOfWork.VillaNumber.GetAll();
+    }
+
+    public VillaNumber GetVillaNumberById(int id)
+    {
+        return _unitOfWork.VillaNumber.Get(u => u.Villa_Number == id);
+    }
+
+    public void UpdateVillaNumber(VillaNumber villaNumber)
+    {
+        _unitOfWork.VillaNumber.Update(villaNumber);
+        _unitOfWork.Save();
+    }
+}
