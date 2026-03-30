b738eaa42b6379020376e1ee975245121f9e1938
Art'm <kasko.artem45@gmail.com>
2026-03-31 00:13:59 +0300

    Create Villa Service Interface

---
+++ WhiteLagoon.Application/Services/Interface/IVillaService.cs
@@ -0,0 +1,12 @@
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Services.Interface;
+
+public interface IVillaService
+{
+    IEnumerable<Villa> GetAllVillas();
+    Villa GetVillaById(int id);
+    void CreateVilla(Villa villa);
+    void UpdateVilla(Villa villa);
+    bool DeleteVilla(int id);
+}
